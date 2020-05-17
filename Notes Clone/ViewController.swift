aaf11ba57a61b587d4ef4e361aade736d3001b4c
Rudolfs Locmelis <rudolfs.locmelis@gmail.com>
2020-05-17 16:45:46 +0100

    Initial Commit

---
+++ Notes Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes Clone
+//
+//  Created by Rudolfs Locmelis on 17/05/2020.
+//  Copyright © 2020 Rudolfs Locmelis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
